3d75d6b7b709e10dade935b4f1d2c569ebca3908
QED0711 <qdizon@gmail.com>
2019-05-20 07:07:34 -0400

    added client side form validation to use signup. still no password encryption

--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -1,50 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 import postAPI from '../js/postAPI'
 
 const UserSignUp = () => {
     
+    let [userCredentials, setUserCredentials] = useState({email: null, password: null, passwordConfirm: null })
+
     const handleUserCreate = (e) => {
         e.preventDefault()
-
+        
+        if(userCredentials.password !== userCredentials.passwordConfirm){
+            window.alert("The password and confirmation password you entered do not match");
+            return
+        }
+        
+        postAPI("userCreate", userCredentials)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+    
+    const updateUserCredientials = (e) => {
+        
         const email = document.getElementById("userEmail").value
         const password = document.getElementById("userPassword").value
         const passwordConfirm = document.getElementById("userPasswordConfirm").value
+        const credentials = {email, password, passwordConfirm}
+        setUserCredentials(credentials)
 
-        
-        const newUser = {
-            email,
-            password,
-            passwordConfirm
+        checkPasswordMatch(credentials)
+    }
+
+    const checkPasswordMatch = (credentials) => {
+        if(credentials.password !== credentials.passwordConfirm && credentials.passwordConfirm.length > 0){
+            document.getElementById("userPasswordConfirm").classList = "form-field-error"
+        } else {
+            document.getElementById("userPasswordConfirm").classList = "form-field-pass"
         }
-        debugger
-        postAPI("userCreate", newUser)
-            .then(res => {
-                console.log(res);
-            })
-            .catch(err => {
-                console.log(err)
-            })
     }
 
     return(
 
-        <form id="user-signup-form" class="user-form" onSubmit={handleUserCreate}>  
+        <form id="user-signup-form" class="user-form" onSubmit={handleUserCreate} onChange={updateUserCredientials}>  
             <label for="userEmail">Email:</label>
             <br/>
-            <input type="email" id="userEmail" />
+            <input type="email" id="userEmail" required />
 
             <br/>
 
             <label for="userPassword">Password:</label>
             <br/>
-            <input type="password" id="userPassword" />
+            <input type="password" id="userPassword" required />
             
             <br/>
 
             <label for="userPasswordConfirm">Confirm Password:</label>
             <br/>
-            <input type="password" id="userPasswordConfirm" />
+            <input type="password" id="userPasswordConfirm" required />
 
             <br/>
 
